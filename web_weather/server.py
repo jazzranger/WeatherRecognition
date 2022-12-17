6e8e331f6ec1c3584a47ccf54d5e3d76dd3c3e1d
en.zadorin <en.zadorin@tensor.ru>
2022-12-17 16:35:03 +0300

    fix predict

--- web_weather/server.py
+++ web_weather/server.py
@@ -3,7 +3,7 @@ from flask_cors import CORS, cross_origin
 from img_tools import img_tools
 from flask import request
 import json
-from torch import load, max, sum, device, tensor, cat
+import torch
 from torchvision import datasets, models, transforms
 from PIL import Image
 import io
@@ -11,6 +11,13 @@ import io
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+
+WEATHERS = {
+    0: 'cloudy',
+    1: 'rain',
+    2: 'shine',
+    3: 'sunrise',
+}
 
 
 @app.route('/download')
@@ -34,20 +41,18 @@ def predict(*args, **kwargs):
         imageBinaryBytes = request.files['file'].read()
         imageStream = io.BytesIO(imageBinaryBytes)
         imageFile = Image.open(imageStream)
-
-        model = load('model.pth', map_location=device('cpu'))
-
-        transform = transforms.Compose([
+        data_transformer = transforms.Compose([
+            transforms.ToTensor(),
             transforms.Resize(256),
             transforms.CenterCrop(244),
-            transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-            ])
+        ])
+        image = data_transformer(imageFile)
+        imgs = torch.stack([image])
+        model = torch.load('model.pth', map_location=torch.device('cpu'))
+        outputs = model(imgs)
+        preds = torch.argmax(outputs, -1)
+        return json.dumps({'result': f'Result: {WEATHERS.get(preds.item())}'})
 
-        image = transform(imageFile)
-
-        predict = model(image)
-        print(predict)
-    return json.dumps({'result': predict})
 
 app.run()
